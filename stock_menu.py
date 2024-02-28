b58f61e983e8291fe6db7bd668a0c26a51e2c470
aka-Cairo <matthew.harper33@gmail.com>
2024-02-28 00:07:10 -0500

    Added a couple more files for comparison's sake

--- stock_menu.py
+++ stock_menu.py
@@ -52,20 +52,8 @@ menu = {
 
 # 1. Set up order list. Order list will store a list of dictionaries for
 # menu item name, item price, and quantity ordered
-customer_order = []
-"""customer_order = list[]
-  {
-    "Item name": "string",
-    "Price": float,
-    "Quantity": int
-  },
-  {
-    "Item name": "string",
-    "Price": float,
-    "Quantity": int
-  },
-]
-"""
+
+
 # Launch the store and present a greeting to the customer
 print("Welcome to the variety food truck.")
 
@@ -130,37 +118,32 @@ while place_order:
                     }
                     i += 1
             # 2. Ask customer to input menu item number
-            item_number = input("Please enter the number of the item you'd like to order: ")
+
+
             # 3. Check if the customer typed a number
-            if item_number.isdigit():
+
                 # Convert the menu selection to an integer
-                item_number = int(item_number)
+
+
                 # 4. Check if the menu selection is in the menu items
-                if 1 <= item_number <= len(menu_items):
+
                     # Store the item name as a variable
-                    item_name = menu_items[item_number]
+
+
                     # Ask the customer for the quantity of the menu item
-                    quantity = input("How many would you like? ")
+
+
                     # Check if the quantity is a number, default to 1 if not
-                    if quantity.isdigit():
-                        quantity = int(quantity)
-                    else:
-                        quantity = 1
+
+
                     # Add the item name, price, and quantity to the order list
-                    order_item = {
-                        "Item name": menu_items[item_number]["Item name"],
-                        "Price": menu_items[item_number]["Price"],
-                        "Quantity": quantity
-                    }                    
-                    customer_order.append(order_item)
-                    print(customer_order)
-                    break
-                # Tell the customer that their input isn't valid
-                else:
-                    print("Please input a valid number")              
-            # Tell the customer they didn't select a menu option
-            else:
-                print("Your selection was not a sub-option")
+
+
+                    # Tell the customer that their input isn't valid
+
+
+                # Tell the customer they didn't select a menu option
+
         else:
             # Tell the customer they didn't select a menu option
             print(f"{menu_category} was not a menu option.")
@@ -168,30 +151,26 @@ while place_order:
         # Tell the customer they didn't select a number
         print("You didn't select a number.")
 
-"""
-THIS IS THE END OF THE CODE THAT ACTUALLY TAKES AN ORDER AND ADDS IT TO THE
-customer_order dictionary
-"""
-
-while True:
+    while True:
         # Ask the customer if they would like to order anything else
         keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ")
-        # 5. Check the customer's input
-        match keep_ordering.lower():
-            case 'y':
-                place_order = True
-                break
-            case 'n':
-                place_order = False     # Since the customer decided to stop ordering,
-                print("Thank you for your order.") #thank them for their order                                               
-                break  # Exit the keep ordering question loop
-            # Tell the customer to try again
-            case _:
-                print("Please type 'y' or 'n' to continue.") 
 
-"""
-THIS IS THE END OF THE CODE THAT ASKS THE USER TO CONTINUE OR NOT
-"""
+        # 5. Check the customer's input
+
+                # Keep ordering
+
+                # Exit the keep ordering question loop
+
+                # Complete the order
+
+                # Since the customer decided to stop ordering, thank them for
+                # their order
+
+                # Exit the keep ordering question loop
+
+
+                # Tell the customer to try again
+
 
 # Print out the customer's order
 print("This is what we are preparing for you.\n")
